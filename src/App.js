3bc1ab8af8fad921b0d20896e1a1aa1361e96ee6
achtenberg167 <achtenberg16@gmail.com.br>
2022-02-07 19:03:50 -0300

    adiciona o projeto

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
-import Login from './pages/Login';
-import Search from './pages/Search';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import ProfileEdit from './pages/ProfileEdit';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import Album from "./pages/Album";
+import Favorites from "./pages/Favorites";
+import ProfileEdit from "./pages/ProfileEdit";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-
           <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route path="/search" component={ Search } />
+            <Route exact path='/trybe-tunes' component={Login} />
+            <Route path='/search' component={Search} />
             <Route
-              path="/album/:id"
-              component={
-                ({ match: { params } }) => <Album { ...params } />
-              }
+              path='/album/:id'
+              component={({ match: { params } }) => <Album {...params} />}
             />
-            <Route path="/favorites" component={ Favorites } />
+            <Route path='/favorites' component={Favorites} />
             <Route
-              path="/profile/edit"
-              render={ (route) => (
-                <ProfileEdit { ...route } />) }
+              path='/profile/edit'
+              render={(route) => <ProfileEdit {...route} />}
             />
-            <Route path="/profile" component={ Profile } />
-            <Route path="/*" component={ NotFound } />
-
+            <Route path='/profile' component={Profile} />
+            <Route path='/*' component={NotFound} />
           </Switch>
-
         </BrowserRouter>
-      </div>);
+      </div>
+    );
   }
 }
 
